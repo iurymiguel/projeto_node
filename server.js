f5f423ef8896ec03658b299bc71803a05e3f6890
Andre <andre.luiz@gec.inatel.br>
2018-09-08 14:42:10 -0300

    4 endpoints feitos

--- server.js
+++ server.js
@@ -3,30 +3,52 @@ var server = restify.createServer();
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
-var toddyList = [{ name: 'toddy 1999', val: 'R$ 500,00' }, { name: 'toddy 2000', val: 'R$ 400,00' }];
+var toddyList = [{ id: 1, lote: '1A', conteudo: 200, validade: '25/09/2018' }, { id: 2, lote: '1B', conteudo: 100, validade: '30/09/2018' }];
 
 function getToddys(req, res, next) {
-    const pos = req.params.pos;
-    const toddy = (pos && pos >= 0 && pos < toddyList.length) ? toddyList[pos] : toddyList;
+    const id = req.params.id;
+    let toddy;
+    if (id) {
+        toddy = toddyList.find((toddy) => toddy.id == id);
+        if (!toddy) {
+            toddy = 'Não encontrado, fera!';
+        }
+    } else {
+        toddy = toddyList;
+    }
+
     res.send(toddy)
     next();
 }
 
 function insertToddy(req, res, next) {
+    toddyList.push(req.body);
+    res.send(toddyList);
     next();
-
 }
 
 function updateToddy(req, res, next) {
+    const body = req.body;
+    toddyList.forEach((toddy) => {
+        if(toddy.id == body.id){
+            toddyList[toddyList.indexOf(toddy)] = body;
+        }
+    });
+    res.send(toddyList);
     next();
 }
 
 function deleteToddy(req, res, next) {
+    const id = req.body.id;
+    toddyList.splice(toddyList.indexOf(toddyList.find((toddy) => toddy.id == id)), 1);
+    res.send(toddyList);
     next();
 }
 
-server.get('/toddy/getToddy/:pos', getToddys);
+server.get('/toddy/getToddy/:id', getToddys);
 server.post('/toddy/insertToddy', insertToddy);
+server.post('/toddy/updateToddy', updateToddy);
+server.post('/toddy/deleteToddy', deleteToddy);
 
 var port = process.env.PORT || 3000;
 
